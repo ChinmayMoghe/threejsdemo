a31f924d94eeeaa49cd9810dbf39805294393660
chinmay <chinmay11.moghe@gmail.com>
2019-07-05 17:31:15 +0530

    updated app.js

--- js/app.js
+++ js/app.js
@@ -3,16 +3,16 @@ var scene = new THREE.Scene();
  scene.add( light );          
 var camera = new THREE.PerspectiveCamera( 95, window.innerWidth/window.innerHeight, 0.1, 1000 );             var renderer = new THREE.WebGLRenderer();             renderer.setSize( window.innerWidth, window.innerHeight );             document.body.appendChild( renderer.domElement );
  var geometry = new THREE.SphereGeometry( 50, 50, 50 ); 
- var material = new THREE.PointsMaterial({size:0.1}); 
- var points = new THREE.Points(geometry, material);
+ //var material = new THREE.PointsMaterial({size:0.1}); 
+ //var points = new THREE.Points(geometry, material);
 //Just to change the material of the object
-//  var material = new THREE.MeshBasicMaterial({
-//      color:0xff0000,
-//      wireframe:true,
-//      transparent:true,
-//      opacity:1    
-//  });
-//  var points=new THREE.Mesh(geometry,material);
+ var material = new THREE.MeshBasicMaterial({
+     color:0xff0000,
+     wireframe:true,
+     transparent:true,
+     opacity:1    
+ });
+ var points=new THREE.Mesh(geometry,material);
  points.position.z=-590;
  scene.add( points);          
 var animate = function () {                 
